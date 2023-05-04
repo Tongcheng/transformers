077ef4ce9887b3fdcdd20d578f7a865ea8e9af95
Ubuntu <ubuntu@ip-172-31-48-232.us-west-2.compute.internal>
2023-05-04 05:04:14 +0000

    add debug

--- src/transformers/models/llama/convert_llama_weights_to_hf.py
+++ src/transformers/models/llama/convert_llama_weights_to_hf.py
@@ -249,6 +249,7 @@ def write_tokenizer(tokenizer_path, input_tokenizer_path):
         tokenizer_class = LlamaTokenizer if LlamaTokenizerFast is None else LlamaTokenizerFast
         print(f"Saving a {tokenizer_class.__name__} to {tokenizer_path}.")
         tokenizer = tokenizer_class(input_tokenizer_path)
+        print("debug 0")
         tokenizer.save_pretrained(tokenizer_path)
     except Exception as e:
         print(f"write_tokenizer() e={e}")
